d7bb4927021d5b9a531fae7d86c073664998c334
meesha.uwu <trann8202@gmail.com>
2025-04-22 17:59:36 +0700

    update list of group

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,14 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import WaitingRoomScreen from "./screens/WaitingRoomScreen";
 import AnswerScreen from "./screens/AnswerScreen";
 import UserManagement from "./screens/admin/UserManagement";
-import ListOfParticipantsScreen from "./screens/ListOfParticipantsScreen";
+import ListOfGroup from "./screens/ListOfGroup";
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<WaitingRoomScreen />} />
         <Route path="/question" element={<AnswerScreen />} />
-        <Route path="/waiting" element={<ListOfParticipantsScreen />} />
+        <Route path="/waiting" element={<ListOfGroup />} />
         <Route path="/userlist" element={<UserManagement />} />
       </Routes>
     </Router>
